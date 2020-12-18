1d01a87f1d71628a7c9b859b11142652c64bdf01
Enzo Volkmann <enzo@exportarts.io>
2020-12-18 16:21:53 +0100

    feat(queue-options): add validation

--- src/queue-options.ts
+++ src/queue-options.ts
@@ -1,15 +1,39 @@
 import { QueueBaseOptions } from 'bullmq';
 
+function throwValidationError(envVarName: string) {
+    throw new Error(`process.env.${envVarName} is not set!`);
+}
+
 export const queueBaseOptions = (
     prefixEnvVar = 'BULLMQ_PREFIX',
     hostEnvVar = 'REDIS_HOST',
     portEnvVar = 'REDIS_PORT',
     passwordEnvVar = 'REDIS_PASSWORD'
-): QueueBaseOptions => ({
-    prefix: process.env[prefixEnvVar],
-    connection: {
-        host: process.env[hostEnvVar],
-        port: +process.env[portEnvVar],
-        password: process.env[passwordEnvVar],
+): QueueBaseOptions => {
+    const prefix = process.env[prefixEnvVar];
+    const host = process.env[hostEnvVar];
+    const port = +process.env[portEnvVar];
+    const password = process.env[passwordEnvVar];
+
+    if (!prefix) {
+        throwValidationError(prefixEnvVar);
     }
-});
+    if (!host) {
+        throwValidationError(hostEnvVar);
+    }
+    if (!port) {
+        throwValidationError(portEnvVar);
+    }
+    if (!password) {
+        throwValidationError(passwordEnvVar);
+    }
+
+    return {
+        prefix,
+        connection: {
+            host,
+            port,
+            password,
+        }
+    };
+};
